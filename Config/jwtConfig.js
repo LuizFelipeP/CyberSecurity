ca851b4610aea777de846e11757e0c7273591dd1
Luiz Pimpa <luizfpompeo82@gmail.com>
2024-11-24 19:56:02 -0300

    Revert "Adiciona .gitignore"
    This reverts commit 7cd5e5d4d306236bf05f7a110e801158d0d14201.

--- Config/jwtConfig.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    secret: process.env.JWT_SECRET || 'seu-segredo-aqui',  // Carrega o segredo do JWT do arquivo .env
-    expiresIn: '1h'  // Expiração do token em 1 hora
-};
--- Utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-dotenv.config();
-
-const generateToken = (userId) => {
-    return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRATION });
-};
-
-module.exports = generateToken;
